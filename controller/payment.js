9a59a95ebb0f7a7366808f13fad1169567042d51
onuohajaphet4 <onuohajaphet4@gmail.com>
2026-03-01 09:22:46 -0800

    completeted customer dashboard

--- controller/payment.js
+++ controller/payment.js
@@ -20,7 +20,7 @@ export const initializePayment = async (req, res) => {
       {
         email: email,
         amount: amountInKobo,
-        callback_url: "http://localhost:5173/payment-success",
+        callback_url: "http://yati-eight.vercel.app/payment-success",
 
         metadata: {
         userId,
